0169f2ecc26e063d3a8ed221ac1a7a01ee8e8278
Bilal Koçak <kocakbilal04@gmail.com>
2021-12-31 16:51:28 +0300

    Create layout system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import './App.scss';
 import {Routes, Route} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Detail from "./Components/Detail/Detail";
+import Layout from "./Components/Layout/Layout";
+import ThemeController from "./Components/ThemeController";
+import {StateProvider} from "./Context/ThemeContext";
 
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/:id" element={<Detail/>}/>
-            </Routes>
+            <StateProvider>
+                <ThemeController>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/:id" element={<Detail/>}/>
+                        </Routes>
+                    </Layout>
+                </ThemeController>
+            </StateProvider>
+
         </div>
     );
 }
