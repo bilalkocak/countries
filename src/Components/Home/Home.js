c1f69945c43129441895d1da74fc4762daa7fbe8
Bilal Koçak <kocakbilal04@gmail.com>
2022-01-09 07:44:27 +0300

    Add style file and use constant for api url

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Card from "../Card/Card";
+import {API_URL} from "../../constant";
+import './Home.scss'
 
 const Home = () => {
     const [countries, setCountries] = useState([])
     useEffect(() => {
-        axios.get('https://restcountries.com/v3.1/all').then(function (response) {
+        axios.get(`${API_URL}/all`).then(function (response) {
             setCountries(response.data)
         })
             .catch(function (error) {
