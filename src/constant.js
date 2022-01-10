14924b81f8f3e9ba60f59bd8fb2ec53dfb88f73b
Bilal Koçak <kocakbilal04@gmail.com>
2022-01-10 09:23:26 +0300

    Add continent

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,28 @@
 export const API_URL = "https://restcountries.com/v2"
+
+export const CONTINENT = [
+    {
+        value: "",
+        label: "Filter by Region",
+    },
+    {
+        value: "africa",
+        label: "Africa"
+    },
+    {
+        value: "americas",
+        label: "America"
+    },
+    {
+        value: "asia",
+        label: "Asia"
+    },
+    {
+        value: "europe",
+        label: "Europe"
+    },
+    {
+        value: "oceania",
+        label: "Oceania"
+    }
+]
