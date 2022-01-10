503904ecf8eb84a5bdb5564a669895b838988f17
Bilal Koçak <kocakbilal04@gmail.com>
2022-01-10 08:16:59 +0300

    Add svgs

---
+++ src/Components/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as ArrowBack } from "./ArrowBack";
+export { default as MoonSc } from "./MoonSc";
+export { default as Moon } from "./Moon";
+export { default as SearchSvgrepoCom } from "./SearchSvgrepoCom";
