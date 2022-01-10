4a0a1a61a70280a4a00b26b5c1d94fc29258e41f
Bilal Koçak <kocakbilal04@gmail.com>
2022-01-10 12:57:20 +0300

    Refactor filtering

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react';
+
 import Card from "../Card/Card";
-import './Home.scss'
 import Search from "../Search/Search";
-import {fetchAll, filterByContinent, searchCountry} from "../../service";
 import CustomSelect from "../CustomSelect/CustomSelect";
+
 import {CONTINENT} from "../../constant";
+import {fetchAll, filterByContinent, searchCountry} from "../../service";
+
+import './Home.scss'
 
 const Home = () => {
     const [countries, setCountries] = useState([])
@@ -16,29 +19,6 @@ const Home = () => {
     }, [])
 
 
-    useEffect(() => {
-        const _search = () => {
-            searchCountry(search)
-                .then(response => {
-                    setCountries(response.data)
-                })
-                .catch(error => console.log(error))
-        }
-        search.length > 2 ? _search() : _fetchAll();
-    }, [search])
-
-    useEffect(() => {
-        const _fetchFiltered = () => {
-            filterByContinent(continent.value)
-                .then(response => {
-                    setCountries(response.data)
-                })
-                .catch(error => console.log(error))
-        }
-
-        continent.value ? _fetchFiltered() : _fetchAll();
-    }, [continent])
-
     const _fetchAll = () => {
         fetchAll()
             .then(response => {
@@ -48,22 +28,58 @@ const Home = () => {
     }
 
 
+    const handleChangeSearch = (value) => {
+        if (value.length === 0) {
+            _fetchAll();
+            setSearch(value)
+        } else {
+            setSearch(value)
+            setContinent(CONTINENT[0]);
+            searchCountry(value)
+                .then(response => {
+                    if (response.data.status !== 404) {
+                        setCountries(response.data)
+                    } else setCountries([])
+                })
+                .catch(error => console.log(error))
+        }
+    }
+
+    const _fetchFiltered = (_continent) => {
+        if (_continent.value === CONTINENT[0].value) {
+            _fetchAll();
+            return;
+        }
+        filterByContinent(_continent.value)
+            .then(response => {
+                setContinent(_continent)
+                setCountries(response.data)
+                setSearch("")
+            })
+            .catch(error => console.log(error))
+    }
+
+
     return (
         <div className={'home-container'}>
             <div className={'home-filter'}>
-                <Search onChange={setSearch} value={search}/>
+                <Search onChange={handleChangeSearch} value={search}/>
                 <div className={'home-select'}>
                     <CustomSelect placeholder={'Filter by Region'}
                                   options={CONTINENT}
                                   selectedOption={continent}
-                                  handleChange={e => setContinent(e)}/>
+                                  handleChange={e => _fetchFiltered(e)}/>
                 </div>
             </div>
             <div className={'cards'}>
                 {
-                    countries.map(country => {
-                        return <Card country={country} key={country.name}/>
-                    })
+                    countries.length > 0 ?
+                        countries.map(country => {
+                            return <Card country={country} key={country.name}/>
+                        }) :
+                        <div>
+                            Country not found
+                        </div>
                 }
             </div>
         </div>
